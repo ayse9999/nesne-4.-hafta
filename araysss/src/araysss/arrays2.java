4b68a5ceca413b335c788913b3b5f023009b4336
aidata <aidata@10.121.3.207>
2022-10-27 10:15:03 +0300

    arrays

---
+++ araysss/src/araysss/arrays2.java
@@ -0,0 +1,16 @@
+package araysss;
+
+public class arrays2 {
+
+	public static void main(String[] args) {
+		int[] array = {32,27,64,18,95,14,90,70,60,37};
+		
+	
+	 System.out.printf("%s%8s%n","index","value");
+		 
+		 for(int counter=0; counter<array.length; counter++) {
+			 System.out.printf("%5d%8d%n" , counter, array[counter]);
+		 }
+	}
+
+}
