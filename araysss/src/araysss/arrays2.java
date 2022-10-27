a3d6ef648df3754da6816d02173be64855fc8db4
aidata <aidata@10.121.3.207>
2022-10-27 10:42:13 +0300

    arrays

--- araysss/src/araysss/arrays2.java
+++ araysss/src/araysss/arrays2.java
@@ -4,6 +4,8 @@ public class arrays2 {
 
 	public static void main(String[] args) {
 		int[] array = {32,27,64,18,95,14,90,70,60,37};
+	int gecici;
+	int i;
 		
 	
 	 System.out.printf("%s%8s%n","index","value");
@@ -11,6 +13,22 @@ public class arrays2 {
 		 for(int counter=0; counter<array.length; counter++) {
 			 System.out.printf("%5d%8d%n" , counter, array[counter]);
 		 }
+		 System.out.println("dizinin tersi");
+		 for( i=0 ;i<array.length;i++) {
+			
+			
+			 gecici=array[i];
+			 array [i]= array [array.length-1];
+			 array[array.length-1]=gecici;
+			 
+			
+				 
+			 System.out.println(array[i]+ " ");
+		 }
+		
+	}
 	}
 
-}
+	 
+	
+
